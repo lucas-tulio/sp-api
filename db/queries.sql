44d2d06861d367f7549685e4962b3e95d95ce3d0
Lucas <lucasdnd@gmail.com>
2015-02-28 19:04:35 -0300

    Added avg pop by region

--- db/queries.sql
+++ db/queries.sql
@@ -1,3 +1,5 @@
 -- Municípios, distritos, zonas
 select z.id, if(m.municipio = 'São Paulo', 'São Paulo', 'Outros') municipio, group_concat(d.distrito SEPARATOR ', ') regioes from zonas z inner join municipios m on z.municipio_id = m.id inner join distritos d on z.distrito_id = d.id group by z.id;
 
+-- Média de moradores por domicílio, por região
+select z.id, group_concat(distinct d.distrito SEPARATOR ', ') regioes, avg(sp.no_morad) moradores from zonas z inner join municipios m on z.municipio_id = m.id inner join distritos d on z.distrito_id = d.id inner join sp on sp.zona = z.id group by z.id order by moradores;
