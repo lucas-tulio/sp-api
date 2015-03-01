9e8c19630ce38ca6cd081f8ca7d9206fabc9f357
Lucas <lucasdnd@gmail.com>
2015-03-01 00:18:42 -0300

    Added transportation table definition

--- db/create-tables.sql
+++ db/create-tables.sql
@@ -19,3 +19,8 @@ CREATE TABLE `zonas` (
 `distrito_id` INT(11) UNSIGNED NOT NULL,
 `municipio_id` INT(11) UNSIGNED NOT NULL
 );
+
+CREATE TABLE `transportes` (
+`id` INT(11) UNSIGNED NOT NULL,
+`transporte` VARCHAR(255) NOT NULL
+);
