f8f7736308ebc5f9985610927b7e3fc46e2a99d5
Tulio <tulio@doubleleft.com>
2015-03-04 22:23:23 -0300

    Added flask

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
