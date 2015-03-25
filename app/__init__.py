8960838ed06a3689ccb86fb6e410a3355946d4ec
Tulio <tulio@doubleleft.com>
2015-03-25 19:31:27 -0300

    Added new method

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,11 @@ db = Database()
 def hello():
   return "Hi!"
 
+#
+# Geral
+#
 @app.route('/zonas/<int:zona_id>', methods=['GET'])
-def get_zona(zona_id):
+def get_zona_one(zona_id):
   if zona_id == 0 or zona_id > 31:
     abort(404)
   result = db.get_one("geral", zona_id)
@@ -25,7 +28,21 @@ def get_zonas():
   result = db.get_all("geral")
   return json.dumps(result)
 
+#
+# Automóveis por residência
+#
 
+@app.route('/automoveis_por_residencia/<int:zona_id>', methods=['GET'])
+def get_automoveis_por_residencia_one(zona_id):
+  if zona_id == 0 or zona_id > 31:
+    abort(404)
+  result = db.get_one("automoveis_por_residencia", zona_id)
+  return json.dumps(result)
+
+@app.route('/automoveis_por_residencia', methods=['GET'])
+def get_automoveis_por_residencia():
+  result = db.get_all("automoveis_por_residencia")
+  return json.dumps(result)
 
 #
 # App start
